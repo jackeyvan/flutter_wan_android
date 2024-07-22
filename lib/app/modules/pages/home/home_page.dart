b1dcac96fe0bf89d552218176e32608ad385290e
shacoc <xmpinger@gmail.com>
2024-07-22 17:45:14 +0800

    更新banner卡片库

--- lib/app/modules/pages/home/home_page.dart
+++ lib/app/modules/pages/home/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:card_swiper/card_swiper.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_wan_android/app/const/styles.dart';
 import 'package:flutter_wan_android/app/modules/entity/article_entity.dart';
@@ -51,28 +51,33 @@ class HomePage extends GetRefreshPage<HomeController> {
     );
   }
 
-  Widget buildBanner(List<BannerEntity> banner) => SizedBox(
-        height: 240,
-        child: Card(
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(12))),
-          child: Swiper(
-            itemBuilder: (BuildContext context, int index) {
-              return InkWell(
-                child: CachedNetworkImage(
-                  imageUrl: banner[index].imagePath ?? "",
-                  fit: BoxFit.fill,
-                ),
-                onTap: () => Routes.toNamed(Routes.articleDetail,
-                    args: ArticleEntity(
-                        title: banner[index].title, link: banner[index].url)),
-              );
-            },
-            itemCount: banner.length,
-            pagination: const SwiperPagination(
-                builder: DotSwiperPaginationBuilder(size: 8, activeSize: 8)),
-            // control: const SwiperControl(),
-          ),
+  Widget buildBanner(List<BannerEntity> banner) {
+    return CarouselSlider.builder(
+        options: CarouselOptions(
+          height: 240,
+          viewportFraction: 1,
+          enableInfiniteScroll: true,
+          autoPlay: true,
+          autoPlayInterval: const Duration(seconds: 5),
+          autoPlayAnimationDuration: const Duration(milliseconds: 500),
         ),
-      );
+        itemCount: banner.length,
+        itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
+          return Card(
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12))),
+            child: Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(12))),
+              child: InkWell(
+                  onTap: () => Routes.toNamed(Routes.articleDetail,
+                      args: ArticleEntity(
+                          title: banner[itemIndex].title,
+                          link: banner[itemIndex].url)),
+                  child: CachedNetworkImage(
+                      imageUrl: banner[itemIndex].url ?? "")),
+            ),
+          );
+        });
+  }
 }
